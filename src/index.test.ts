13806334a56341f9df26c96e5483dc93fcde9b4d
Ben Christel <ben.christel@gmail.com>
2025-06-07 16:58:08 -0700

    Delete dummy index files

--- src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import {test, expect, is} from "@benchristel/taste"
-import {hello} from "./index.js"
-
-test("hello", {
-    "returns a greeting"() {
-        expect(hello(), is, "Hello, world!")
-    },
-})
