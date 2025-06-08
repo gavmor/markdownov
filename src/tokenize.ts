ae4f85a0a15d8fcf4969fbaa4226b617b5a5e9d8
Ben Christel <ben.christel@gmail.com>
2025-06-07 18:48:41 -0700

    Inline function

--- src/tokenize.ts
+++ src/tokenize.ts
@@ -17,13 +17,8 @@ const tokenRegex = new RegExp(
 export function *tokenize(text: string): Generator<string> {
     let token: string | undefined
     let rest = text
-    while (token = wholeMatch(tokenRegex, rest)) {
+    while (token = rest.match(tokenRegex)?.[0]) {
         yield token
         rest = rest.slice(token.length)
     }
-}
-
-function wholeMatch(re: RegExp, s: string): string | undefined {
-    const match = s.match(re)
-    return match?.[0]
 }
