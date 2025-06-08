3408467e6f24ae589bb61a608458c0ebb50aa03b
Ben Christel <ben.christel@gmail.com>
2025-06-07 21:02:33 -0700

    MarkovModel: Abstract the order of the model

--- src/markov-model.ts
+++ src/markov-model.ts
@@ -3,40 +3,80 @@ import {tokenize} from "./tokenize.js"
 
 const END = ""
 
+export interface Context {
+    id(): string;
+    afterObserving(token: string): Context;
+    empty(): Context;
+    textBoundary(): string[];
+    isEndOfText(): boolean;
+}
+
+class Order1 implements Context {
+    token = END
+
+    id(): string {
+        return this.token
+    }
+
+    afterObserving(token: string): Context {
+        this.token = token
+        return this
+    }
+
+    empty(): Context {
+        return new Order1()
+    }
+
+    textBoundary(): string[] {
+        return [END]
+    }
+
+    isEndOfText(): boolean {
+        return this.token === END
+    }
+}
+
 export class MarkovModel {
     private transitions: Record<string, string[]> = {}
+    private initialContext: Context
 
-    constructor(private rng: () => number) {}
+    constructor(
+        private rng: () => number,
+        initialContext = new Order1(),
+    ) {
+        this.initialContext = initialContext.empty()
+    }
 
     train(text: string) {
-        // TODO: hardcoded order
-        const tokens = [END, ...tokenize(text), END]
-        // TODO: hardcoded order
-        for (let i = 1; i < tokens.length; i++) {
+        const textBoundary = this.initialContext.textBoundary()
+        const tokens = [
+            ...textBoundary,
+            ...tokenize(text),
+            ...textBoundary,
+        ]
+        let context = this.initialContext
+        for (let i = textBoundary.length; i < tokens.length; i++) {
             const token = tokens[i]
-            // TODO: hardcoded context type
-            const previous = tokens[i - 1]
-            this.transitions[previous] ??= []
-            this.transitions[previous].push(token)
+            this.transitions[context.id()] ??= []
+            this.transitions[context.id()].push(token)
+            context = context.afterObserving(token)
         }
     }
 
     generate(): string {
-        let generated = [END]
-        // TODO: hardcoded order
-        let context = END
+        let generated = this.initialContext.textBoundary()
+        let context = this.initialContext
         // TODO: magic number
         for (let i = 0; i < 42; i++) {
             const next = this.predictFrom(context)
             generated.push(next)
-            if (next === END) break
-            // TODO: hardcoded order
-            context = next
+            context = context.afterObserving(next)
+            if (context.isEndOfText()) break
         }
         return generated.join("")
     }
 
-    private predictFrom(context: string): string {
-        return pick(this.rng, this.transitions[context] ?? [END])
+    private predictFrom(context: Context): string {
+        return pick(this.rng, this.transitions[context.id()] ?? [END])
     }
 }
