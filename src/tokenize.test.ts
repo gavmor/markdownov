70e38e7e38fc50511966af89319a7b6137993ab8
Ben Christel <ben.christel@gmail.com>
2025-06-07 17:23:37 -0700

    Implement basic tokenizer

--- src/tokenize.test.ts
+++ src/tokenize.test.ts
@@ -6,4 +6,39 @@ test("tokenize", {
         const tokens = [...tokenize("")]
         expect(tokens, equals, [])
     },
+
+    "emits one word as a token"() {
+        const tokens = [...tokenize("word")]
+        expect(tokens, equals, ["word"])
+    },
+
+    "emits a space as a token"() {
+        const tokens = [...tokenize(" ")]
+        expect(tokens, equals, [" "])
+    },
+
+    "keeps runs of space together"() {
+        const tokens = [...tokenize("   ")]
+        expect(tokens, equals, ["   "])
+    },
+
+    "keeps a word and following space together"() {
+        const tokens = [...tokenize("a ")]
+        expect(tokens, equals, ["a "])
+    },
+
+    "splits a word and preceding space apart"() {
+        const tokens = [...tokenize(" a")]
+        expect(tokens, equals, [" ", "a"])
+    },
+
+    "separates a sequence of words"() {
+        const tokens = [...tokenize("a b c")]
+        expect(tokens, equals, ["a ", "b ", "c"])
+    },
+
+    "treats punctuation as space"() {
+        const tokens = [...tokenize("Hello, world!")]
+        expect(tokens, equals, ["Hello, ", "world!"])
+    },
 })
--- src/tokenize.ts
+++ src/tokenize.ts
@@ -1,3 +1,15 @@
-export function *tokenize(text: string): Generator<string> {
+const tokenRegex = /^\w*\W*/
 
+export function *tokenize(text: string): Generator<string> {
+    let token: string | undefined
+    let rest = text
+    while (token = wholeMatch(tokenRegex, rest)) {
+        yield token
+        rest = rest.slice(token.length)
+    }
+}
+
+function wholeMatch(re: RegExp, s: string): string | undefined {
+    const match = s.match(re)
+    return match?.[0]
 }
