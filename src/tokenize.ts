26e6010d5b4ee5ae56a717a287a2d25219819e8c
Ben Christel <ben.christel@gmail.com>
2025-06-07 18:34:36 -0700

    Refactor

--- src/tokenize.ts
+++ src/tokenize.ts
@@ -1,4 +1,15 @@
-const tokenRegex = /^([\u2E80-\u2EFF\u3200-\u32FF\u3300-\u33FF\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF\uFE30-\uFE4F]|[0-9\p{L}]*)[^0-9\p{L}]*/u
+const cjkChars = [
+    // See: https://www.regular-expressions.info/unicode.html#category
+    String.raw`\u2E80-\u2EFF`, // CJK Radicals Supplement
+    String.raw`\u3200-\u32FF`, // Enclosed CJK Letters and Months
+    String.raw`\u3300-\u33FF`, // CJK Compatibility
+    String.raw`\u3400-\u4DBF`, // CJK Unified Ideographs Extension A
+    String.raw`\u4E00-\u9FFF`, // CJK Unified Ideographs
+    String.raw`\uF900-\uFAFF`, // CJK Compatibility Ideographs
+    String.raw`\uFE30-\uFE4F`, // CJK Compatibility Forms
+].join("")
+
+const tokenRegex = new RegExp(String.raw`^([${cjkChars}]|[0-9\p{L}]*)[^0-9\p{L}]*`, "u")
 
 export function *tokenize(text: string): Generator<string> {
     let token: string | undefined
