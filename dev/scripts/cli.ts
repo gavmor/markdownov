f2dcf6c590f4a6257d82819c6f2666d7c3cd93aa
Ben Christel <ben.christel@gmail.com>
2025-06-14 17:44:54 -0700

    Fix import

--- dev/scripts/cli.ts
+++ dev/scripts/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env bun
 import * as fs from "fs/promises"
 import {MarkovModel} from "../../src/markov/markov-model"
-import {Lossy} from "../../src/markov/lossy"
+import {Lossy} from "../../src/markov/order/lossy"
 
 const paths = process.argv.slice(2)
 
